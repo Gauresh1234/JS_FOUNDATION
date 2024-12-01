f01bd6b0e14569e2d1eac0e1d98603fde83e629e
Gauresh1234 <kotiangaureshxb@gmail.com>
2024-12-01 14:25:27 +0530

    JAVASCRIPT

--- FOUNDATION.js
+++ FOUNDATION.js
@@ -1,19 +1,20 @@
-// VARIABLES//
+// VARIABLES //
 var name="Gauresh";
 var age=19;
 console.log(name, age)
 
-// HOISTING//
+// HOISTING //
 console.log(greeting);
 var greeting="Hello!";
 
-// DATATYPES//
+// DATATYPES //
 var num=42;
 var name="Alice";
 var isLoggedIn=false;
 var x;
 var y=null;
 console.log(num,name,isLoggedIn,x,y)
+
 
 // ARITHMATIC OPERATOR//
 // ADDITION//
@@ -45,6 +46,69 @@ console.log(num);
 var num=19;
 num--;
 console.log(num);
+
+
+// COMPARISON OPERATOR//
+// EQUALTO//
+console.log(5=="5");
+
+// STRICT.EQUALTO//
+console.log(5==="5");
+
+//NOT.EQUALTO//
+console.log(6!="5");
+
+//STRICTNOT.EQUALTO//
+console.log(6!=="6");
+
+// GREATERTHAN//
+console.log(8>5);
+
+// LESSTHAN//
+console.log(8<5);
+
+
+// LOGICAL OPERATOR//
+// AND//
+console.log(true&&false);
+
+// OR//
+console.log(true||false);
+
+// NOT//
+console.log(!true);
+
+
+// ASSIGNMENT OPERATOR//
+// ASSIGNMENT//
+var x=10;
+console.log(x);
+
+// ADD&ASSIGN//
+var x=5;
+x+=8;
+console.log(x);
+
+// SUBTRACT&ASSIGN//
+var x=9;
+x-=4;
+console.log(x);
+
+// MULTIPLY&ASSIGN//
+var x=17;
+x*=8;
+console.log(x);
+
+// DIVIDE&ASSIGN//
+var x=19;
+x/=76;
+console.log(x);
+
+// OBJECTS //
+
+
+
+
 
 
 
