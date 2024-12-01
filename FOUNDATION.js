49e2bbf5b3299e6361c7520b79a5e10b3f840b8e
Gauresh1234 <kotiangaureshxb@gmail.com>
2024-12-01 13:47:33 +0530

    JAVASCRIPT

---
+++ FOUNDATION.js
@@ -0,0 +1,50 @@
+// VARIABLES//
+var name="Gauresh";
+var age=19;
+console.log(name, age)
+
+// HOISTING//
+console.log(greeting);
+var greeting="Hello!";
+
+// DATATYPES//
+var num=42;
+var name="Alice";
+var isLoggedIn=false;
+var x;
+var y=null;
+console.log(num,name,isLoggedIn,x,y)
+
+// ARITHMATIC OPERATOR//
+// ADDITION//
+var sum=5+8;
+console.log(sum);
+
+// SUBTRACT//
+var difference=17-6;
+console.log(difference);
+
+// MULTIPLICATION//
+var product=67*98;
+console.log(product);
+
+// DIVISION//
+var quotient=7/56;
+console.log(quotient);
+
+// MODULUS//
+var remainder=9%30;
+console.log(remainder);
+
+// INCREMENT//
+var num=10;
+num++;
+console.log(num);
+
+// DECREMENT//
+var num=19;
+num--;
+console.log(num);
+
+
+
